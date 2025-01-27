95cd7f0561c7ecd67bc86307f1551d92b79f05e1
Agustin Arnez <74320809+aguss-afk@users.noreply.github.com>
2025-01-27 19:34:21 -0400

    ensenhar a adri

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+
+#define raya() cout << endl << "====================================" << endl
+#define dbg(x) cerr << #x << ": " << x << endl;
+
+using namespace std;
+using ll = long long;
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18 + 5;
+
+void solve(){
+    cout << "Hello World";    
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+}
