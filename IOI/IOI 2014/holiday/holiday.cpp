5584cc17339bf27836ba1ca7153c944035f6df37
Agustin Arnez <itsmeagus2@gmail.com>
2025-02-15 15:03:57 -0400

    idk

--- IOI/IOI 2014/holiday/holiday.cpp
+++ IOI/IOI 2014/holiday/holiday.cpp
@@ -2,27 +2,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
-ll findMaxAttraction(int n, int start, int d, int attraction[]){
-    ll ans = 0;
-    for (int l = start; l >= 0; l--){
-        priority_queue<int> q;
-        ll sum = 0;
-        for (int r = l; r < n; r++) {
-            q.push(-attraction[r]);
-            sum += attraction[r];
-            if (r < start){
-                continue;
-            }
-            int k = d - min(start - 2 * l + r, 2 * r - start - l);
-            if (k <= 0){
-                break;
-            }
-            while (q.size() > k) {
-                sum += q.top();
-                q.pop();
-            }
-            ans = max(ans, sum);
-        }
+
+int n;
+vector<ll> arr;
+ll ans = 0;
+vector<bool> vis;
+vector<vector<ll>> dpcaca;
+
+ll r(int d, int curr){
+    if(d <= 0 or curr >= n){
+        return 0;
+    }
+    if(dpcaca[d][curr] != -1){
+        return dpcaca[d][curr];
+    }
+    ll n1 = arr[curr] + r(d - 2, curr + 1);
+    ll n2 = r(d - 1, curr + 1);
+    return dpcaca[d][curr] = max(n1, n2);
+}
+
+ll l(int d, int curr){
+    if(d <= 0 or curr < 0){
+        return 0;
+    }
+    if(dpcaca[d][curr] != -1){
+        return dpcaca[d][curr];
+    }
+    ll n1 = arr[curr] + l(d - 2, curr - 1);
+    ll n2 = r(d - 1, curr - 1);
+    return dpcaca[d][curr] = max(n1, n2);
+}
+long long int findMaxAttraction(int N, int start, int d, int attraction[]) {
+    n = N;
+    arr.assign(n, 0);
+    copy(attraction, attraction + n, arr.begin());
+    for(int i = 0; i < n; i++){
+        dpcaca.assign(d + 5, vector<ll>(n + 5, -1));
+        dpcaca.clear();
+        ans = max(ans, l(d - abs(start - i), i));
+        dpcaca.assign(d + 5, vector<ll>(n + 5, -1));
+        dpcaca.clear();
+        ans = max(ans, r(d - abs(start - i), i));
     }
     return ans;
 }
